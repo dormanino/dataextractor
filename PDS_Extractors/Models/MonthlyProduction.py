02317c5f162f7f83d5e836cfbd9bb5b71af22822
dormanino <victor.ravagnani@icloud.com>
2018-07-31 01:26:06 -0300

    Tweaks

--- PDS_Extractors/Models/MonthlyProduction.py
+++ PDS_Extractors/Models/MonthlyProduction.py
@@ -2,10 +2,20 @@ from PDS_Extractors.Models.QVVProduction import QVVProduction
 
 
 class MonthlyProduction:
-
     def __init__(self, month: str, qvv_production_list: [QVVProduction]):
         self.month = month
         self.qvv_production_list = qvv_production_list
+
+    @classmethod
+    def from_dict(cls, datadict):
+        return cls(
+            datadict[MonthlyProduction.JSONKeys.month],
+            list(map(QVVProduction.from_dict, datadict[MonthlyProduction.JSONKeys.qvv_production_list]))
+        )
+
+    class JSONKeys:
+        month = "month"
+        qvv_production_list = "data"
 
     def total_volume(self) -> int:
         return sum(qvv_production.volume for qvv_production in self.qvv_production_list)
@@ -19,14 +29,3 @@ class MonthlyProduction:
             else:
                 families[key] = qvv_production.volume
         return families
-
-    @classmethod
-    def from_dict(cls, datadict):
-        return cls(
-            datadict[MonthlyProduction.JSONKeys.month],
-            list(map(QVVProduction.from_dict, datadict[MonthlyProduction.JSONKeys.qvv_production_list]))
-        )
-
-    class JSONKeys:
-        month = "month"
-        qvv_production_list = "data"
