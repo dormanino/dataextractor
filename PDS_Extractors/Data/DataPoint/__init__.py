afd16a6024b59590cd556b12c8c6aca5b65e4713
dormanino <victor.ravagnani@icloud.com>
2018-07-31 03:31:02 -0300

    Better data point

--- PDS_Extractors/Data/DataPoint/__init__.py
+++ PDS_Extractors/Data/DataPoint/__init__.py
@@ -5,13 +5,17 @@ from PDS_Extractors.Helpers.LatestFileVersion import LatestFileVersion
 class DataPoint:
     path = Settings.PROJECT_DATA_FILES
 
+    # Suffixes
+    SUFFIX_data_agrmz_vehicles = '_PDS_AGRMZ_parsed_final_Vehicles'
+
+    # Paths
     data_0e = LatestFileVersion.latest_file_version('json', '_PDS_0E', current=path)
     data_acc = LatestFileVersion.latest_file_version('json', '_PDS_ACC', current=path)
 
     data_02 = LatestFileVersion.latest_file_version('json', '_PDS_02', current=path)
     data_kgs_agr_vehicles = LatestFileVersion.latest_file_version('json', '_PDS_KGS_AGR_Vehicles', current=path)
     data_agrmz_raw_vehicles = LatestFileVersion.latest_file_version('json', '_PDS_BM´s_AGRMZ_Vehicles', current=path)
-    data_agrmz_vehicles = LatestFileVersion.latest_file_version('json', '_PDS_AGRMZ_parsed_final_Vehicles', current=path)
+    data_agrmz_vehicles = LatestFileVersion.latest_file_version('json', SUFFIX_data_agrmz_vehicles, current=path)
 
     data_03 = LatestFileVersion.latest_file_version('json', '_PDS_03', current=path)
     data_kgs_agr_aggregates = LatestFileVersion.latest_file_version('json', '_PDS_KGS_AGR_Aggregates', current=path)
