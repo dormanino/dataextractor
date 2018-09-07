c3b87472c75b8e3ad83d2ff4d4191b4edd447ef8
dormanino <victor.ravagnani@icloud.com>
2018-09-07 11:37:59 -0300

    inclusion of partials into 12mpp analysis

--- BPM_STAR_Extractors/12MPP_Mainframe_Extractor/__init__.py
+++ BPM_STAR_Extractors/12MPP_Mainframe_Extractor/__init__.py
@@ -71,6 +71,8 @@ class ProPresum:
         for dates in dates_header_list:
             month, year = PartitionStringinDigitsandNonDigits.Partition.partition(dates)
             dates_from_header.append((month, year))
+        months_from_header = [month for month, year in [data for data in dates_from_header]]
+        years_from_header = set(year for month, year in [data for data in dates_from_header] if year != '')
 
         TOTALS_DECLARATION = 'TOTAL FINAL'
         oper_eof = True
@@ -80,6 +82,7 @@ class ProPresum:
         totals_line_list = []
         dicto = {}
         main_operation_dict = {'variant_data': []}
+        partials_main_operation_dict = {'variant_data': []}
         full_volume_data = {}
         months_volume_dict = {}
 
@@ -92,6 +95,75 @@ class ProPresum:
                 if self.mainframe.string_get(line, 1, 80).strip() != '':
                     data_line_list.append((self.mainframe.string_get(line, 1, 80), self.mainframe.string_get(line + 1, 1, 80)))
 
+            if TOTALS_DECLARATION not in self.mainframe.string_get(19, 1, 80):
+                counter = 0
+                main_data_str = self.mainframe.string_get(4, 1, 80)
+                variant = main_data_str[1:29]
+                oper_eof_partial = True
+
+                while oper_eof_partial:
+                    for line in range(7, 18, 2):  # range 7-18 in steps of 2 lines copy the variant information's
+                        if not self.mainframe.string_get(line, 1, 80).strip() == '' and\
+                                not self.mainframe.string_get(line + 1, 1, 80).strip() == '':
+
+                            partial_volume = self.mainframe.string_get(line + 1, 1, 80).strip().split(' ')
+                            partial_volume[:] = [item for item in partial_volume if item != '']
+                            partials_volume_dict = dict(zip(months_from_header, partial_volume))
+                            partials_period_totals = {"TOTAL": partials_volume_dict["TOTAL"]}
+                            partials_main_data_str = self.mainframe.string_get(line, 1, 80).strip()
+                            partials_register_information = partials_main_data_str[1:26].strip().split('-')
+                            partials_order_lacation_information = partials_main_data_str[26:80].strip.split('-')
+
+                            if len(years_from_header) == 1:
+                                partials_volume_dict.pop("TOTAL")
+                                months_volume_dict = partials_volume_dict
+                                volume_data = {'register_number': partials_register_information[0],
+                                               'register_name': partials_register_information[1],
+                                               'order_location_number_cerep': partials_order_lacation_information[0],
+                                               'order_location_name': partials_order_lacation_information[1],
+                                               'months': months_volume_dict, 'totals': partials_period_totals}
+                                full_volume_data = {"year": years_from_header, "data": volume_data}
+                            else:
+                                partials_volume_dict.pop("TOTAL")
+                                swap_volume_dict = partials_volume_dict
+                                for year_from_header in years_from_header:
+                                    for month, year in dates_from_header:
+                                        if month in swap_volume_dict.keys() and year == year_from_header:
+                                            months_volume_dict = {'key': month, 'value': partials_volume_dict[month]}
+                                    volume_data = {'register_number': partials_register_information[0],
+                                                   'register_name': partials_register_information[1],
+                                                   'order_location_number_cerep': partials_order_lacation_information[0],
+                                                   'order_location_name': partials_order_lacation_information[1],
+                                                   'months': months_volume_dict, 'totals': partials_period_totals}
+                                    full_volume_data = {"year": year_from_header, "data": volume_data}
+
+                            main_data_str = self.mainframe.string_get(4, 1, 80)
+                            variant = main_data_str[1:29]
+                            variant_representation = main_data_str[29:58]
+
+                            partials_main_operation_dict['variant_data'].append({'variant': variant.replace(' ', ''),
+                                                                                 'variant_representation': variant_representation.strip(),
+                                                                                 'volume_data': full_volume_data
+                                                                                 })
+                            counter += 1
+
+                    if TOTALS_DECLARATION in self.mainframe.string_get(19, 1, 80):
+                        oper_eof_partial = False
+                        # in order for the flow of data be maintained, the next variant have to be queued. Then reintroduced into (22,9) for
+                        # the screen to be reloaded on the analysed variant. This is why the next block of code is maintained
+                        # in the flow with the inclusion of a erase statement to the before mentioned position
+                        self.mainframe.move_to(22, 9)
+                        self.mainframe.send_eraseEOF()
+
+                    self.mainframe.move_to(24, 80)
+                    self.mainframe.send_enter()
+                    self.mainframe.wait_for_field()
+
+                self.mainframe.send_string(variant, 22, 9)
+                self.mainframe.move_to(24, 80)
+                self.mainframe.send_enter()
+                self.mainframe.wait_for_field()
+
             # the data has partial data in the screen
             if TOTALS_DECLARATION in self.mainframe.string_get(19, 1, 80):
                 # first dict
@@ -102,12 +174,8 @@ class ProPresum:
                 data_line_list = []
 
                 # second dict (test)
-
                 totals_volume = totals_line.strip().split(' ')
                 totals_volume[:] = [item for item in totals_volume if item != '']
-                # dates_from_header[0] is only the month in the string
-                months_from_header = [month for month, year in [data for data in dates_from_header]]
-                years_from_header = set(year for month, year in [data for data in dates_from_header] if year != '')
                 volume_dict = dict(zip(months_from_header, totals_volume))
                 period_totals = {"TOTAL": volume_dict["TOTAL"]}
                 if len(years_from_header) == 1:
