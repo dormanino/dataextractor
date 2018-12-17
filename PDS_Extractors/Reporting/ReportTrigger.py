7067d27f1b5ccf1fc1c208a4488d470b0b4e69f9
dormanino <vravagn@gmail.com>
2018-12-17 15:10:29 -0200

    included saa's for parts for bm into report trigger

--- PDS_Extractors/Reporting/ReportTrigger.py
+++ PDS_Extractors/Reporting/ReportTrigger.py
@@ -1,25 +1,32 @@
 import json
 
-from Globus_Data.Models.Extractors import LoadGlobusData
-from PDS_Extractors.Models.Baumuster.BaumusterInfo import BaumusterInfo
+from PDS_Extractors.Data.DataPoint import DataPoint
+
 from PDS_Extractors.Models.Plant import Plant
-from PDS_Extractors.Reports.FamilyParts import FamilyPartsReport
+from PDS_Extractors.Models.Production.Production import Production
+from PDS_Extractors.Models.Baumuster.BaumusterInfo import BaumusterInfo
+from PDS_Extractors.Models.Baumuster.BaumusterCollection import BaumusterCollection
+from PDS_Extractors.Models.Part.ComponentsCollection import ComponentsCollection
+from PDS_Extractors.Models.DataSource.TechDocDataSource import TechDocDataSource
+
+from Globus_Data.Models.Extractors import LoadGlobusData
+
 from PDS_Extractors.TechDoc.Extraction.BaumusterComponentsExtractor import BaumusterComponentsExtractor
 from PDS_Extractors.TechDoc.Extraction.QVVComponentsExtractor import QVVComponentsExtractor
-from PDS_Extractors.Data.DataPoint import DataPoint
-from PDS_Extractors.Models.Baumuster.BaumusterCollection import BaumusterCollection
-from PDS_Extractors.Models.DataSource.TechDocDataSource import TechDocDataSource
-from PDS_Extractors.Models.Part.ComponentsCollection import ComponentsCollection
-from PDS_Extractors.Models.Production.Production import Production
+
+from PDS_Extractors.TechDoc.Validation.DueDate.DueDateStatus import DueDateStatus
+
 from PDS_Extractors.Reporting.ReportGroupings import ReportGroupings
 from PDS_Extractors.Reporting.ReportOutput import ReportOutput
 from PDS_Extractors.Reporting.ReportType import ReportType
-from PDS_Extractors.Reports.CostAnalysisReport import CostAnalysisReport
+
 from PDS_Extractors.Reports.EPUSplitReport import EPUSplitReport
+from PDS_Extractors.Reports.CostAnalysisReport import CostAnalysisReport
 from PDS_Extractors.Reports.TechDocStatusReport import TechDocStatusReport
-from PDS_Extractors.TechDoc.Validation.DueDate.DueDateStatus import DueDateStatus
+from PDS_Extractors.Reports.FamilyParts import FamilyPartsReport
 from PDS_Extractors.Reports.SAAsExtractionReport import SAAsExtractionReport
 from PDS_Extractors.Reports.OptionalsPartNumberReport import OptionalsPartNumberReport
+from PDS_Extractors.Reports.PartsExtractionReport import PartsExtractionReport
 
 
 class ReportTrigger:
@@ -110,3 +117,12 @@ class ReportTrigger:
             elif report_type is ReportType.ExtractSAAFromAGRMZ_JDF:
                 return report.run(Plant.JDF)
 
+        # SAA's and parts for bm
+        if report_type in ReportGroupings.extract_saa_and_parts_from_bm:
+            # TODO: pass list from outside of the method
+            bm_list = ["C963403", "C963414", "C963424", "C963425"]
+            report = PartsExtractionReport(self.tech_doc_data_source, bm_list)
+            if report_type is ReportType.ExtractSAAandPartsfromBM_SBC:
+                return report.run(Plant.SBC)
+            elif report_type is ReportType.ExtractSAAandPartsfromBM_JDF:
+                return report.run(Plant.JDF)
